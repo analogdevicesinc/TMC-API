390855ef41934a120d0f78f19ad95fc24d7fa90e
Leonard Kugis <leonard@kug.is>
2019-04-12 13:06:46 +0200

    TMC2660
    Added macros and constants to extract address and value from a datagram.
    This is required for the cover + shadow register mechanism with TMC43XX.

--- tmc/ic/TMC2660/TMC2660_Macros.h
+++ tmc/ic/TMC2660/TMC2660_Macros.h
@@ -2,12 +2,16 @@
  * TMC2660_Macros.h
  *
  *  Created on: 01.10.2018
- *      Author: tmctest
+ *      Author: LK
  */
 
 #ifndef TMC_IC_TMC2660_TMC2660_MACROS_H_
 #define TMC_IC_TMC2660_TMC2660_MACROS_H_
 
+#include "TMC2660_Constants.h"
 
+#define TMC2660_IS_WRITE(datagram) ((datagram) >> (TMC2660_ADDRESS_SHIFT + 3))
+#define TMC2660_ADDRESS(datagram) (((datagram) >> TMC2660_ADDRESS_SHIFT) & TMC2660_ADDRESS_MASK)
+#define TMC2660_VALUE(datagram) ((datagram) & TMC2660_VALUE_MASK)
 
 #endif /* TMC_IC_TMC2660_TMC2660_MACROS_H_ */
