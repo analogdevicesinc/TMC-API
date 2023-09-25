0fb5052c1a655f3d18a5de744663acd6190a98e6
dennismalcorps <112079884+dennismalcorps@users.noreply.github.com>
2023-09-25 14:35:16 +0200

    Fix possible macro redefinition warning

--- tmc/helpers/Macros.h
+++ tmc/helpers/Macros.h
@@ -24,7 +24,9 @@
 #endif
 
 // Static Array length
-#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+#ifndef ARRAY_SIZE
+	#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+#endif
 
 // Generic mask/shift macros
 #define FIELD_GET(data, mask, shift) \
