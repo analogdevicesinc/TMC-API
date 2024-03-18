fd8c15b7b191ac8b26e172e52e4428e77be3b74d
Daniyal Hussain <Daniyal.Hussain@analog.com>
2024-03-18 23:58:47 +0100

    TMC5271: Reworked register access and register field access mechanism

--- tmc/ic/TMC5271/TMC5271.c
+++ tmc/ic/TMC5271/TMC5271.c
@@ -2,7 +2,7 @@
 * Copyright © 2017 TRINAMIC Motion Control GmbH & Co. KG
 * (now owned by Analog Devices Inc.),
 *
-* Copyright © 2023 Analog Devices Inc. All Rights Reserved. This software is
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved. This software is
 * proprietary & confidential to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
@@ -10,6 +10,127 @@
 #include <tmc/ic/TMC5271/TMC5271.h>
 
 
+static int32_t readRegisterSPI(uint16_t icID, uint8_t address);
+static void writeRegisterSPI(uint16_t icID, uint8_t address, int32_t value);
+static int32_t readRegisterUART(uint16_t icID, uint8_t registerAddress);
+static void writeRegisterUART(uint16_t icID, uint8_t registerAddress, int32_t value);
+
+
+
+int32_t tmc5271_readRegister(uint16_t icID, uint8_t address)
+{
+    TMC5271BusType bus = tmc5271_getBusType(icID);
+
+    if(bus == IC_BUS_SPI)
+    {
+        return readRegisterSPI(icID, address);
+    }
+    else if (bus == IC_BUS_UART)
+    {
+        return readRegisterUART(icID, address);
+    }
+
+    //ToDo: Error handling
+    return -1;
+}
+
+void tmc5271_writeRegister(uint16_t icID, uint8_t address, int32_t value)
+{
+    TMC5271BusType bus = tmc5271_getBusType(icID);
+
+    if(bus == IC_BUS_SPI)
+    {
+        writeRegisterSPI(icID, address, value);
+    }
+    else if(bus == IC_BUS_UART)
+    {
+        writeRegisterUART(icID, address, value);
+    }
+}
+
+int32_t readRegisterSPI(uint16_t icID, uint8_t address)
+{
+    uint8_t data[5] = { 0 };
+
+    // clear write bit
+    data[0] = address & TMC_ADDRESS_MASK;
+
+    // Send the read request
+    tmc5271_readWriteSPI(icID, &data[0], sizeof(data));
+
+    // Rewrite address and clear write bit
+    data[0] = address & TMC_ADDRESS_MASK;
+
+    // Send another request to receive the read reply
+    tmc5271_readWriteSPI(icID, &data[0], sizeof(data));
+
+    return ((int32_t)data[1] << 24) | ((int32_t) data[2] << 16) | ((int32_t) data[3] <<  8) | ((int32_t) data[4]);
+}
+
+void writeRegisterSPI(uint16_t icID, uint8_t address, int32_t value)
+{
+    uint8_t data[5] = { 0 };
+
+    data[0] = address | TMC_WRITE_BIT;
+    data[1] = 0xFF & (value>>24);
+    data[2] = 0xFF & (value>>16);
+    data[3] = 0xFF & (value>>8);
+    data[4] = 0xFF & (value>>0);
+
+    // Send the write request
+    tmc5271_readWriteSPI(icID, &data[0], sizeof(data));
+}
+
+int32_t readRegisterUART(uint16_t icID, uint8_t registerAddress)
+{
+    uint8_t data[8] = { 0 };
+
+    registerAddress = registerAddress & TMC_ADDRESS_MASK;
+
+    data[0] = 0x05;
+    data[1] = tmc5271_getNodeAddress(icID); //targetAddressUart;
+    data[2] = registerAddress;
+    data[3] = tmc_CRC8(data, 3, 1);
+
+    if (!tmc5271_readWriteUART(icID, &data[0], 4, 8))
+        return 0;
+
+    // Byte 0: Sync nibble correct?
+    if (data[0] != 0x05)
+        return 0;
+
+    // Byte 1: Master address correct?
+    if (data[1] != 0xFF)
+        return 0;
+
+    // Byte 2: Address correct?
+    if (data[2] != registerAddress)
+        return 0;
+
+    // Byte 7: CRC correct?
+    if (data[7] != tmc_CRC8(data, 7, 1))
+        return 0;
+
+    return ((uint32_t)data[3] << 24) | ((uint32_t)data[4] << 16) | (data[5] << 8) | data[6];
+}
+
+void writeRegisterUART(uint16_t icID, uint8_t registerAddress, int32_t value)
+{
+    uint8_t data[8];
+
+    data[0] = 0x05;
+    data[1] = (uint8_t)tmc5271_getNodeAddress(icID); //targetAddressUart;
+    data[2] = registerAddress | TMC_WRITE_BIT;
+    data[3] = (value >> 24) & 0xFF;
+    data[4] = (value >> 16) & 0xFF;
+    data[5] = (value >> 8 ) & 0xFF;
+    data[6] = (value      ) & 0xFF;
+    data[7] = tmc_CRC8(data, 7, 1);
+
+    tmc5271_readWriteUART(icID, &data[0], 8, 0);
+}
+
+/***************** The following code is TMC-EvalSystem specific and needs to be commented out when working with other MCUs e.g Arduino*****************************/
 // Initialize a TMC5271 IC.
 // This function requires:
 //     - tmc5271: The pointer to a TMC5271TypeDef struct, which represents one IC
--- tmc/ic/TMC5271/TMC5271.h
+++ tmc/ic/TMC5271/TMC5271.h
@@ -2,7 +2,7 @@
 * Copyright © 2017 TRINAMIC Motion Control GmbH & Co. KG
 * (now owned by Analog Devices Inc.),
 *
-* Copyright © 2023 Analog Devices Inc. All Rights Reserved. This software is
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved. This software is
 * proprietary & confidential to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
@@ -17,9 +17,79 @@
 
 // Helper macros
 #define TMC5271_FIELD_READ(tdef, address, mask, shift) \
-	FIELD_GET(tmc5271_readInt(tdef, address), mask, shift)
+    FIELD_GET(tmc5271_readInt(tdef, address), mask, shift)
 #define TMC5271_FIELD_WRITE(tdef, address, mask, shift, value) \
-	(tmc5271_writeInt(tdef, address, FIELD_SET(tmc5271_readInt(tdef, address), mask, shift, value)))
+    (tmc5271_writeInt(tdef, address, FIELD_SET(tmc5271_readInt(tdef, address), mask, shift, value)))
+
+// Typedefs
+
+// Amount of CRC tables available
+// Each table takes ~260 bytes (257 bytes, one bool and structure padding)
+#define CRC_TABLE_COUNT 2
+
+typedef enum {
+    IC_BUS_SPI,
+    IC_BUS_UART,
+    IC_BUS_WLAN,
+} TMC5271BusType;
+
+// => TMC-API wrapper
+extern void tmc5271_readWriteSPI(uint16_t icID, uint8_t *data, size_t dataLength);
+extern bool tmc5271_readWriteUART(uint16_t icID, uint8_t *data, size_t writeLength, size_t readLength);
+extern TMC5271BusType tmc5271_getBusType(uint16_t icID);
+extern uint8_t tmc5271_getNodeAddress(uint16_t icID);
+// => TMC-API wrapper
+
+int32_t tmc5271_readRegister(uint16_t icID, uint8_t address);
+void tmc5271_writeRegister(uint16_t icID, uint8_t address, int32_t value);
+
+typedef struct
+{
+    uint32_t mask;
+    uint8_t shift;
+    uint8_t address;
+    bool isSigned;
+} RegisterField;
+
+static inline uint32_t field_extract(uint32_t data, RegisterField field)
+{
+    uint32_t value = (data & field.mask) >> field.shift;
+
+    if (field.isSigned)
+    {
+        // Apply signedness conversion
+        uint32_t baseMask = field.mask >> field.shift;
+        uint32_t signMask = baseMask & (~baseMask >> 1);
+        value = (value ^ signMask) - signMask;
+    }
+
+    return value;
+}
+
+static inline uint32_t field_read(uint16_t icID, RegisterField field)
+{
+    uint32_t value = tmc5271_readRegister(icID, field.address);
+
+    return field_extract(value, field);
+}
+
+static inline uint32_t field_update(uint32_t data, RegisterField field, uint32_t value)
+{
+    return (data & (~field.mask)) | ((value << field.shift) & field.mask);
+}
+
+static inline void field_write(uint16_t icID, RegisterField field, uint32_t value)
+{
+    uint32_t regValue = tmc5271_readRegister(icID, field.address);
+
+    regValue = field_update(regValue, field, value);
+
+    tmc5271_writeRegister(icID, field.address, regValue);
+}
+
+/***************** The following code is TMC-EvalSystem specific and needs to be commented out when working with other MCUs e.g Arduino*****************************/
+
+#include "tmc/helpers/API_Header.h"
 
 // Typedefs
 typedef struct
