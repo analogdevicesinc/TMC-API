2cdde58595874b87924078d9698dc20fb6027e59
Daniyal Hussain <Daniyal.Hussain@analog.com>
2024-05-15 11:52:30 +0200

    TMC2262: Added new register access and field access mechanism.

--- tmc/ic/TMC2262/TMC2262.c
+++ tmc/ic/TMC2262/TMC2262.c
@@ -6,6 +6,57 @@
 
 #include "TMC2262.h"
 
+//NEW_CODE_BEGIN
+static int32_t readRegisterSPI(uint16_t icID, uint8_t address);
+static void writeRegisterSPI(uint16_t icID, uint8_t address, int32_t value);
+
+
+int32_t tmc2262_readRegister(uint16_t icID, uint8_t address)
+{
+	return readRegisterSPI(icID, address);
+}
+
+void tmc2262_writeRegister(uint16_t icID, uint8_t address, int32_t value)
+{
+	writeRegisterSPI(icID, address, value);
+}
+
+int32_t readRegisterSPI(uint16_t icID, uint8_t address)
+{
+	uint8_t data[5] = { 0 };
+
+	// clear write bit
+	data[0] = address & TMC2262_ADDRESS_MASK;
+
+	// Send the read request
+	tmc2262_readWriteSPI(icID, &data[0], sizeof(data));
+
+	// Rewrite address and clear write bit
+	data[0] = address & TMC2262_ADDRESS_MASK;
+
+	// Send another request to receive the read reply
+	tmc2262_readWriteSPI(icID, &data[0], sizeof(data));
+
+	return ((int32_t)data[1] << 24) | ((int32_t) data[2] << 16) | ((int32_t) data[3] <<  8) | ((int32_t) data[4]);
+}
+
+void writeRegisterSPI(uint16_t icID, uint8_t address, int32_t value)
+{
+	uint8_t data[5] = { 0 };
+
+	data[0] = address | TMC2262_WRITE_BIT;
+	data[1] = 0xFF & (value>>24);
+	data[2] = 0xFF & (value>>16);
+	data[3] = 0xFF & (value>>8);
+	data[4] = 0xFF & (value>>0);
+
+	// Send the write request
+	tmc2262_readWriteSPI(icID, &data[0], sizeof(data));
+}
+
+/***************** The following code is TMC-EvalSystem specific and needs to be commented out when working with other MCUs e.g Arduino*****************************/
+//NEW_CODE_END
+
 extern void tmc2262_readWriteArray(uint8_t channel, uint8_t *data, size_t length);
 
 // Writes (x1 << 24) | (x2 << 16) | (x3 << 8) | x4 to the given address
--- tmc/ic/TMC2262/TMC2262.h
+++ tmc/ic/TMC2262/TMC2262.h
@@ -8,10 +8,66 @@
 #define TMC_IC_TMC2262_H_
 
 #include "tmc/helpers/API_Header.h"
-#include "tmc/helpers/Constants.h"
-#include "TMC2262_Constants.h"
-#include "TMC2262_fields.h"
-#include "TMC2262_registers.h"
+#include "TMC2262_HW_Abstraction.h"
+
+//NEW_CODE_BEGIN
+#define DEFAULT_MOTOR  0
+
+// => TMC-API wrapper
+extern void tmc2262_readWriteSPI(uint16_t icID, uint8_t *data, size_t dataLength);
+extern uint8_t tmc2262_getNodeAddress(uint16_t icID);
+// => TMC-API wrapper
+
+int32_t tmc2262_readRegister(uint16_t icID, uint8_t address);
+void tmc2262_writeRegister(uint16_t icID, uint8_t address, int32_t value);
+void tmc2262_rotateMotor(uint16_t icID, uint8_t motor, int32_t velocity);
+///
+typedef struct
+{
+    uint32_t mask;
+    uint8_t shift;
+    uint8_t address;
+    bool isSigned;
+} RegisterField;
+
+static inline uint32_t field_extract(uint32_t data, RegisterField field)
+{
+    uint32_t value = (data & field.mask) >> field.shift;
+
+    if (field.isSigned)
+    {
+        // Apply signedness conversion
+        uint32_t baseMask = field.mask >> field.shift;
+        uint32_t signMask = baseMask & (~baseMask >> 1);
+        value = (value ^ signMask) - signMask;
+    }
+
+    return value;
+}
+
+static inline uint32_t field_read(uint16_t icID, RegisterField field)
+{
+    uint32_t value = tmc2262_readRegister(icID, field.address);
+
+    return field_extract(value, field);
+}
+
+static inline uint32_t field_update(uint32_t data, RegisterField field, uint32_t value)
+{
+    return (data & (~field.mask)) | ((value << field.shift) & field.mask);
+}
+
+static inline void field_write(uint16_t icID, RegisterField field, uint32_t value)
+{
+    uint32_t regValue = tmc2262_readRegister(icID, field.address);
+
+    regValue = field_update(regValue, field, value);
+
+    tmc2262_writeRegister(icID, field.address, regValue);
+}
+/***************** The following code is TMC-EvalSystem specific and needs to be commented out when working with other MCUs e.g Arduino*****************************/
+
+//NEW_CODE_END
 
 // Helper macros
 #define TMC2262_FIELD_READ(tdef, address, mask, shift) \
