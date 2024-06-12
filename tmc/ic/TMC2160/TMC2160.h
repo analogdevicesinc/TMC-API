1d4e7a3b7ff36e81e5d541555a2a62c51b5aced0
ME <marvin.ellebrecht@analog.com>
2024-06-12 17:07:43 +0200

    TMC2160: (Rework) make fields naming more concise

--- tmc/ic/TMC2160/TMC2160.h
+++ tmc/ic/TMC2160/TMC2160.h
@@ -31,7 +31,7 @@ typedef struct
     bool isSigned;
 } RegisterField;
 
-static inline uint32_t field_extract(uint32_t data, RegisterField field)
+static inline uint32_t tmc2160_field_extract(uint32_t data, RegisterField field)
 {
     uint32_t value = (data & field.mask) >> field.shift;
 
@@ -46,23 +46,23 @@ static inline uint32_t field_extract(uint32_t data, RegisterField field)
     return value;
 }
 
-static inline uint32_t field_read(uint16_t icID, RegisterField field)
+static inline uint32_t tmc2160_field_read(uint16_t icID, RegisterField field)
 {
 	uint32_t value = tmc2160_readRegister(icID, field.address);
 
-    return field_extract(value, field);
+    return tmc2160_field_extract(value, field);
 }
 
-static inline uint32_t field_update(uint32_t data, RegisterField field, uint32_t value)
+static inline uint32_t tmc2160_field_update(uint32_t data, RegisterField field, uint32_t value)
 {
     return (data & (~field.mask)) | ((value << field.shift) & field.mask);
 }
 
-static inline void field_write(uint16_t icID, RegisterField field, uint32_t value)
+static inline void tmc2160_field_write(uint16_t icID, RegisterField field, uint32_t value)
 {
 	uint32_t regValue = tmc2160_readRegister(icID, field.address);
 
-	regValue = field_update(regValue, field, value);
+	regValue = tmc2160_field_update(regValue, field, value);
 
     tmc2160_writeRegister(icID, field.address, regValue);
 }
