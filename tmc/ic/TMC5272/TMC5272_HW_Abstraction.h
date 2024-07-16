df3fa18b651aa4c33a79ffc8fbd038f949c19084
Daniyal Hussain <Daniyal.Hussain@analog.com>
2024-07-16 15:32:42 +0200

    TMC5272: Changed the Copyright year.

--- tmc/ic/TMC5272/TMC5272_HW_Abstraction.h
+++ tmc/ic/TMC5272/TMC5272_HW_Abstraction.h
@@ -2,7 +2,7 @@
 * Copyright © 2019 TRINAMIC Motion Control GmbH & Co. KG
 * (now owned by Analog Devices Inc.),
 *
-* Copyright © 2023 Analog Devices Inc. All Rights Reserved.
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
 * This software is proprietary to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
