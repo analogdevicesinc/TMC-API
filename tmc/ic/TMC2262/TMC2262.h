89d94e42b030b11d1ba06e7fbb76a903570a7e1d
Daniyal Hussain <Daniyal.Hussain@analog.com>
2024-08-07 16:33:04 +0200

    TMC2262: Added chip name as a prefix to the field R/W functions.

--- tmc/ic/TMC2262/TMC2262.h
+++ tmc/ic/TMC2262/TMC2262.h
@@ -29,7 +29,7 @@ typedef struct
     bool isSigned;
 } RegisterField;
 
-static inline uint32_t field_extract(uint32_t data, RegisterField field)
+static inline uint32_t tmc2262_fieldExtract(uint32_t data, RegisterField field)
 {
     uint32_t value = (data & field.mask) >> field.shift;
 
@@ -44,23 +44,23 @@ static inline uint32_t field_extract(uint32_t data, RegisterField field)
     return value;
 }
 
-static inline uint32_t field_read(uint16_t icID, RegisterField field)
+static inline uint32_t tmc2262_fieldRead(uint16_t icID, RegisterField field)
 {
     uint32_t value = tmc2262_readRegister(icID, field.address);
 
-    return field_extract(value, field);
+    return tmc2262_fieldExtract(value, field);
 }
 
-static inline uint32_t field_update(uint32_t data, RegisterField field, uint32_t value)
+static inline uint32_t tmc2262_fieldUpdate(uint32_t data, RegisterField field, uint32_t value)
 {
     return (data & (~field.mask)) | ((value << field.shift) & field.mask);
 }
 
-static inline void field_write(uint16_t icID, RegisterField field, uint32_t value)
+static inline void tmc2262_fieldWrite(uint16_t icID, RegisterField field, uint32_t value)
 {
     uint32_t regValue = tmc2262_readRegister(icID, field.address);
 
-    regValue = field_update(regValue, field, value);
+    regValue = tmc2262_fieldUpdate(regValue, field, value);
 
     tmc2262_writeRegister(icID, field.address, regValue);
 }
