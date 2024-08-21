5c55c0391ecff4775ea8defba1f1eaf02002485a
Aysha Sundas <Aysha.Sundas@analog.com>
2024-08-21 14:25:44 +0200

    TMC5160: Fixed spaces in copyright text

--- tmc/ic/TMC5160/TMC5160.c
+++ tmc/ic/TMC5160/TMC5160.c
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright © 2019 TRINAMIC Motion Control GmbH & Co. KG
- * (now owned by Analog Devices Inc.),
- *
- * Copyright © 2024 Analog Devices Inc. All Rights Reserved.
- * This software is proprietary to Analog Devices, Inc. and its licensors.
- *******************************************************************************/
+* Copyright © 2019 TRINAMIC Motion Control GmbH & Co. KG
+* (now owned by Analog Devices Inc.),
+*
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
+* This software is proprietary to Analog Devices, Inc. and its licensors.
+*******************************************************************************/
 
 
 #include "TMC5160.h"
