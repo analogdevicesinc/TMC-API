b792cb57f69f706112e375e8506a5916ca5cacad
ME <marvin.ellebrecht@analog.com>
2024-09-09 15:22:50 +0200

    TMC5041: (Rework Phase 2) change naming for fields

--- tmc/ic/TMC5041/TMC5041.h
+++ tmc/ic/TMC5041/TMC5041.h
@@ -53,7 +53,7 @@ typedef struct
     bool isSigned;
 } RegisterField;
 
-static inline uint32_t field_extract(uint32_t data, RegisterField field)
+static inline uint32_t tmc5041_fieldExtract(uint32_t data, RegisterField field)
 {
     uint32_t value = (data & field.mask) >> field.shift;
 
@@ -68,23 +68,23 @@ static inline uint32_t field_extract(uint32_t data, RegisterField field)
     return value;
 }
 
-static inline uint32_t field_read(uint16_t icID, RegisterField field)
+static inline uint32_t tmc5041_fieldRead(uint16_t icID, RegisterField field)
 {
     uint32_t value = tmc5041_readRegister(icID, field.address);
 
-    return field_extract(value, field);
+    return tmc5041_fieldExtract(value, field);
 }
 
-static inline uint32_t field_update(uint32_t data, RegisterField field, uint32_t value)
+static inline uint32_t tmc5041_fieldUpdate(uint32_t data, RegisterField field, uint32_t value)
 {
     return (data & (~field.mask)) | ((value << field.shift) & field.mask);
 }
 
-static inline void field_write(uint16_t icID, RegisterField field, uint32_t value)
+static inline void tmc5041_fieldWrite(uint16_t icID, RegisterField field, uint32_t value)
 {
     uint32_t regValue = tmc5041_readRegister(icID, field.address);
 
-    regValue = field_update(regValue, field, value);
+    regValue = tmc5041_fieldUpdate(regValue, field, value);
 
     tmc5041_writeRegister(icID, field.address, regValue);
 }
