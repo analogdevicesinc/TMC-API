e9cc52d9bde3a23da29dfb673d006c827a5f301f
ME <marvin.ellebrecht@analog.com>
2024-09-18 15:20:24 +0200

    TMC6200:(rework) replace the register and register field functions add a hardware abstraction file add README

--- tmc/ic/TMC6200/TMC6200.h
+++ tmc/ic/TMC6200/TMC6200.h
@@ -2,7 +2,7 @@
 * Copyright © 2018 TRINAMIC Motion Control GmbH & Co. KG
 * (now owned by Analog Devices Inc.),
 *
-* Copyright © 2023 Analog Devices Inc. All Rights Reserved.
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
 * This software is proprietary to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
@@ -10,18 +10,60 @@
 #ifndef TMC_IC_TMC6200_H_
 #define TMC_IC_TMC6200_H_
 
-#include "tmc/helpers/API_Header.h"
-#include "TMC6200_Register.h"
-#include "TMC6200_Constants.h"
-#include "TMC6200_Fields.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "TMC6200_HW_Abstraction.h"
 
-// Helper macros
-#define TMC6200_FIELD_READ(tdef, address, mask, shift) \
-	FIELD_GET(tmc6200_readInt(tdef, address), mask, shift)
-#define TMC6200_FIELD_UPDATE(tdef, address, mask, shift, value) \
-	(tmc6200_writeInt(tdef, address, FIELD_SET(tmc6200_readInt(tdef, address), mask, shift, value)))
+// => TMC-API wrapper
+extern void tmc6200_readWriteSPI(uint16_t icID, uint8_t *data, size_t dataLength);
+// => TMC-API wrapper
 
-int32_t tmc6200_readInt(uint8_t motor, uint8_t address);
-void tmc6200_writeInt(uint8_t motor, uint8_t address, int32_t value);
+int32_t tmc6200_readRegister(uint16_t icID, uint8_t address);
+void tmc6200_writeRegister(uint16_t icID, uint8_t address, int32_t value);
 
-#endif /* TMC_IC_TMC6630_H_ */
+typedef struct
+{
+    uint32_t mask;
+    uint8_t shift;
+    uint8_t address;
+    bool isSigned;
+} RegisterField;
+
+static inline uint32_t tmc6200_fieldExtract(uint32_t data, RegisterField field)
+{
+    uint32_t value = (data & field.mask) >> field.shift;
+
+    if (field.isSigned)
+    {
+        // Apply signedness conversion
+        uint32_t baseMask = field.mask >> field.shift;
+        uint32_t signMask = baseMask & (~baseMask >> 1);
+        value = (value ^ signMask) - signMask;
+    }
+
+    return value;
+}
+
+static inline uint32_t tmc6200_fieldRead(uint16_t icID, RegisterField field)
+{
+    uint32_t value = tmc6200_readRegister(icID, field.address);
+
+    return tmc6200_fieldExtract(value, field);
+}
+
+static inline uint32_t tmc6200_fieldUpdate(uint32_t data, RegisterField field, uint32_t value)
+{
+    return (data & (~field.mask)) | ((value << field.shift) & field.mask);
+}
+
+static inline void tmc6200_fieldWrite(uint16_t icID, RegisterField field, uint32_t value)
+{
+    uint32_t regValue = tmc6200_readRegister(icID, field.address);
+
+    regValue = tmc6200_fieldUpdate(regValue, field, value);
+
+    tmc6200_writeRegister(icID, field.address, regValue);
+}
+
+#endif /* TMC_IC_TMC6200_H_ */
