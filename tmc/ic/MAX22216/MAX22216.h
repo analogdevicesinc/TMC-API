416df430eef16bacbf50f1aa3d28d1167fe9bcaf
Daniyal Hussain <Daniyal.Hussain@analog.com>
2024-12-17 15:18:31 +0100

    MAX22216: Added new field R/W mechanism.

--- tmc/ic/MAX22216/MAX22216.h
+++ tmc/ic/MAX22216/MAX22216.h
@@ -10,17 +10,18 @@
 #ifndef API_IC_MAX22216_H
 #define API_IC_MAX22216_H
 
-#include "tmc/helpers/API_Header.h"
-#include "MAX22216_Fields.h"
-#include "MAX22216_Register.h"
-#include "MAX22216_Constants.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "MAX22216_HW_Abstraction.h"
 
-// Helper macros
-#define MAX22216_FIELD_READ(tdef, address, mask, shift) \
-        FIELD_GET(max22216_readInt(tdef, address), mask, shift)
-#define MAX22216_FIELD_WRITE(tdef, address, mask, shift, value) \
-        (max22216_writeInt(tdef, address, FIELD_SET(max22216_readInt(tdef, address), mask, shift, value)))
-
+typedef struct
+{
+    uint32_t mask;
+    uint8_t shift;
+    uint8_t address;
+    bool isSigned;
+} RegisterField;
 // Usage note: use 1 TypeDef per IC
 typedef struct {
     uint8_t channel;
@@ -28,17 +29,48 @@ typedef struct {
     uint8_t crc_en;
 } MAX22216TypeDef;
 
+static inline uint16_t max22216_fieldExtract(uint16_t data, RegisterField field)
+{
+    uint16_t value = (data & field.mask) >> field.shift;
+
+    if (field.isSigned)
+    {
+        // Apply signedness conversion
+        uint16_t baseMask = field.mask >> field.shift;
+        uint16_t signMask = baseMask & (~baseMask >> 1);
+        value             = (value ^ signMask) - signMask;
+    }
 uint8_t max22216_CRC(uint8_t *data, size_t length);
 
+    return value;
+}
+
+static inline uint16_t max22216_fieldRead(uint16_t icID, RegisterField field)
+{
+    uint16_t value = max22216_readRegister(icID, field.address);
+
+    return max22216_fieldExtract(value, field);
+}
 void max22216_writeDatagram(MAX22216TypeDef *max22216, uint8_t address, uint8_t x1, uint8_t x2);
 void max22216_writeInt(MAX22216TypeDef *max22216, uint8_t address, int16_t value);
 int32_t max22216_readInt(MAX22216TypeDef *max22216, uint8_t address);
 
+static inline uint16_t max22216_fieldUpdate(uint16_t data, RegisterField field, uint16_t value)
+{
+    return (data & (~field.mask)) | ((value << field.shift) & field.mask);
+}
 void max22216_writeIntDep(MAX22216TypeDef *max22216, uint8_t address, int32_t value, uint8_t dep_address, int32_t dep_value);
 int32_t max22216_readIntDep(MAX22216TypeDef *max22216, uint8_t address, uint8_t dep_address, int32_t dep_value);
 
+static inline void max22216_fieldWrite(uint16_t icID, RegisterField field, uint16_t value)
+{
+    uint16_t regValue = max22216_readRegister(icID, field.address);
+
+    regValue = max22216_fieldUpdate(regValue, field, value);
 void max22216_init(MAX22216TypeDef *max22216, uint8_t channel);
 
+    max22216_writeRegister(icID, field.address, regValue);
+}
 uint8_t max22216_getSlaveAddress(const MAX22216TypeDef *max22216);
 void max22216_setSlaveAddress(MAX22216TypeDef *max22216, uint8_t slaveAddress);
 
