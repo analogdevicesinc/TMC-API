eab046085305993469efb6014de8bbd972f40a25
Daniyal Hussain <Daniyal.Hussain@analog.com>
2024-12-18 15:10:07 +0100

    TMC4361A: Update the year in copyright text.

--- tmc/ic/TMC4361A/TMC4361A.h
+++ tmc/ic/TMC4361A/TMC4361A.h
@@ -1,8 +1,8 @@
 /*******************************************************************************
-* Copyright © 2017 TRINAMIC Motion Control GmbH & Co. KG
+* Copyright © 2019 TRINAMIC Motion Control GmbH & Co. KG
 * (now owned by Analog Devices Inc.),
 *
-* Copyright © 2023 Analog Devices Inc. All Rights Reserved.
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
 * This software is proprietary to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
