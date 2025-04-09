dfa8ac439adf9ec32538672ad55dad6c46b9cfd2
Daniyal Hussain <Daniyal.Hussain@analog.com>
2025-04-09 17:17:11 +0200

    MAX22215: bug fixes in registers R/W functions.

--- tmc/ic/MAX22215/MAX22215.c
+++ tmc/ic/MAX22215/MAX22215.c
@@ -33,34 +33,24 @@ void max22215_writeRegister(uint16_t icID, uint8_t address, int32_t value)
 
 int32_t readRegisterIIC(uint16_t icID, uint8_t address)
 {
-    uint8_t data[6] = { 0 };
+    uint8_t data[3] = { 0 };
 
     data[0] = max22215_getDeviceAddress(icID);
-    data[1] = address;//register address
-
-//    IICMasterWrite(data[0], &data[1], 1);
-//    IICMasterRead(data[0], &data[2], 4);
-
-
+    data[1] = address;  //register address
 
     if(!max22215_readWriteIIC(icID, &data[0], 1,1))
         return 0;
 
-    return ((int32_t)data[2] << 24) | ((int32_t) data[3] << 16) | ((int32_t) data[4] <<  8) | ((int32_t) data[5]);
+    return (int32_t)data[2];
 }
 
 void writeRegisterIIC(uint16_t icID, uint8_t address, int32_t value)
 {
-    uint8_t data[6] = { 0 };
+    uint8_t data[3] = { 0 };
 
     data[0] = max22215_getDeviceAddress(icID);
-    data[1] = address;//register address
-    data[2] = 0xFF & (value>>24);
-    data[3] = 0xFF & (value>>16);
-    data[4] = 0xFF & (value>>8);
-    data[5] = 0xFF & (value>>0);
+    data[1] = address;  //register address
+    data[2] = 0xFF & value;
 
-    // Send the write request
-    //max22215_readWriteIIC(icID, &data[0], 5, 0);
-    IICMasterWrite(data[0], &data[1], 5);
+    IICMasterWrite(data[0], &data[1], 2);
 }
